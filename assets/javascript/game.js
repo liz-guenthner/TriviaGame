5bb6e0abad6fffee0ab9a52dc29b0bab9509ed21
Liz Guenthner <liz.guenthner@t-3.com>
2019-06-21 22:16:54 -0500

    add new images for right, wrong answers and js for logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,30 +41,15 @@ var a4 = [
 ];
 
 var n = 0;
-n++;
+
 var score = 0;
+n++;
+var wrong = 0;
 
 
-// $( 'button.blueButton' ).click(function() {
-//     seconds = 200;
-//     begin();
-// });
-
-// function timer() {
-//     seconds = seconds -1;
-//     if (seconds < 20) {
-//         $('#time').text(seconds);
-//     }
-//     if (seconds < 1) {
-//         window.clearInterval(update);
-//         $('#time').append("<h2>Time's up!</h2>");
-//     }
-// }
-
-// update = setInterval('timer()', 20000);
 
 function begin() {
-    seconds = 20;
+    seconds = 30;
     $('#disappear').text('');
     $('#message').text('');
     $('#question').text(q[0]);
@@ -73,112 +58,20 @@ function begin() {
     $('#option3').html(a3[0]);
     $('#option4').html(a4[0]);
     $('#number').text(n++);
+    runTimer();
 }
 
 function q1c() {
-    $('#answer').html("<div id=\"green\">Correct!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new2()\">Next</button>");
+    // $('#answer').html("<div class=\"green\">Correct!</div>");
+    // $('#option1').text('');
+    // $('#option2').text('');
+    // $('#option3').text('');
+    // $('#option4').text('');
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/question1-r2d2.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new2()\">Next</button>");
     score++;
     $('#score').text(score);
-    // console.log('correct!');
-    // $('#winLoseScreen').show(0).delay(1000).hide(0);
-    // $('#winLoseScreen').show();
-}
-
-function q1i() {
-    $('#answer').html("<div id=\"red\">Incorrect!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new2()\">Next</button>");
-}
-
-function q2c() {
-    $('#answer').html("<div id=\"green\">Correct!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new3()\">Next</button>");
-    score++;
-    $('#score').text(score);
-}
-
-function q2i() {
-    $('#answer').html("<div id=\"red\">Incorrect!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new3()\">Next</button>");
-}
-
-function q3c() {
-    $('#answer').html("<div id=\"green\">Correct!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new4()\">Next</button>");
-    score++;
-    $('#score').text(score);
-}
-
-function q3i() {
-    $('#answer').html("<div id=\"red\">Incorrect!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new4()\">Next</button>");
-}
-
-function q4c() {
-    $('#answer').html("<div id=\"green\">Correct!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new5()\">Next</button>");
-    score++;
-    $('#score').text(score);
-}
-
-function q4i() {
-    $('#answer').html("<div id=\"red001\">Incorrect!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"new5()\">Next</button>");
-}
-
-function q5c() {
-    $('#answer').html("<div id=\"green\">Correct!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"end()\">End of Quiz</button>");
-    score++;
-    $('#score').text(score);
-}
-
-function q5i() {
-    $('#answer').html("<div id=\"red\">Incorrect!</div>");
-    $('#option1').text('');
-    $('#option2').text('');
-    $('#option3').text('');
-    $('#option4').text('');
-    $('#next').html("<button class=\"blueButton\" onclick=\"end()\">End of Quiz</button>");
-}
-
-function new2() {
     $('#question').text(q[1]);
     $('#option1').html(a1[1]);
     $('#option2').html(a2[1]);
@@ -189,7 +82,30 @@ function new2() {
     $('#number').text(n++);
 }
 
-function new3() {
+function q1i() {
+    // $('#answer').html("<div class=\"red\">Incorrect!</div>");
+    // $('#option1').text('');
+    // $('#option2').text('');
+    // $('#option3').text('');
+    // $('#option4').text('');
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new2()\">Next</button>");
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/wrong-answer.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    wrong++;
+    $('#wrong').text(wrong);
+}
+
+function q2c() {
+    // $('#answer').html("<div class=\"green\">Correct!</div>");
+    // $('#option1').text('');
+    // $('#option2').text('');
+    // $('#option3').text('');
+    // $('#option4').text('');
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/question2-tauntaun.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new3()\">Next</button>");
+    score++;
+    $('#score').text(score);
     $('#question').text(q[2]);
     $('#option1').html(a1[2]);
     $('#option2').html(a2[2]);
@@ -200,7 +116,30 @@ function new3() {
     $('#number').text(n++);
 }
 
-function new4() {
+function q2i() {
+    // $('#answer').html("<div class=\"red\">Incorrect!</div>");
+    // $('#option1').text('');
+    // $('#option2').text('');
+    // $('#option3').text('');
+    // $('#option4').text('');
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new3()\">Next</button>");
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/wrong-answer.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    wrong++;
+    $('#wrong').text(wrong);
+}
+
+function q3c() {
+    $('#answer').html("<div class=\"green\">Correct!</div>");
+    $('#option1').text('');
+    $('#option2').text('');
+    $('#option3').text('');
+    $('#option4').text('');
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/question3-luke.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new4()\">Next</button>");
+    score++;
+    $('#score').text(score);
     $('#question').text(q[3]);
     $('#option1').html(a1[3]);
     $('#option2').html(a2[3]);
@@ -211,7 +150,30 @@ function new4() {
     $('#number').text(n++);
 }
 
-function new5() {
+function q3i() {
+    // $('#answer').html("<div class=\"red\">Incorrect!</div>");
+    // $('#option1').text('');
+    // $('#option2').text('');
+    // $('#option3').text('');
+    // $('#option4').text('');
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new4()\">Next</button>");
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/wrong-answer.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    wrong++;
+    $('#wrong').text(wrong);
+}
+
+function q4c() {
+    $('#answer').html("<div class=\"green\">Correct!</div>");
+    $('#option1').text('');
+    $('#option2').text('');
+    $('#option3').text('');
+    $('#option4').text('');
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/question4-darth.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new5()\">Next</button>");
+    score++;
+    $('#score').text(score);
     $('#question').text(q[4]);
     $('#option1').html(a1[4]);
     $('#option2').html(a2[4]);
@@ -222,8 +184,31 @@ function new5() {
     $('#number').text(n++);
 }
 
-function end() {
-    $('#message').html("You got " + score + " right!");
+function q4i() {
+    // $('#answer').html("<div class=\"red001\">Incorrect!</div>");
+    // $('#option1').text('');
+    // $('#option2').text('');
+    // $('#option3').text('');
+    // $('#option4').text('');
+    // $('#next').html("<button class=\"blueButton\" onclick=\"new5()\">Next</button>");
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/wrong-answer.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    wrong++;
+    $('#wrong').text(wrong);
+}
+
+function q5c() {
+    $('#answer').html("<div class=\"green\">Correct!</div>");
+    $('#option1').text('');
+    $('#option2').text('');
+    $('#option3').text('');
+    $('#option4').text('');
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/question5-the-force.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    // $('#next').html("<button class=\"blueButton\" onclick=\"end()\">End of Quiz</button>");
+    score++;
+    $('#score').text(score);
+    $('#message').html("You got " + score + " right!<br>You got " + wrong + " wrong!");
     $('#answer').text('');
     $('#question').text('');
     $('#option1').text('');
@@ -231,11 +216,116 @@ function end() {
     $('#option3').text('');
     $('#option4').text('');
     $('#next').html("<button class=\"blueButton\" onclick=\"repeat()\">Play Again</button>");
+    stopTimer();
 }
+
+function q5i() {
+    // $('#answer').html("<div class=\"red\">Incorrect!</div>");
+    // $('#option1').text('');
+    // $('#option2').text('');
+    // $('#option3').text('');
+    // $('#option4').text('');
+    // $('#next').html("<button class=\"blueButton\" onclick=\"end()\">End of Quiz</button>");
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/wrong-answer.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    wrong++;
+    $('#wrong').text(wrong);
+}
+
+// function new2() {
+//     runTimer();
+//     $('#question').text(q[1]);
+//     $('#option1').html(a1[1]);
+//     $('#option2').html(a2[1]);
+//     $('#option3').html(a3[1]);
+//     $('#option4').html(a4[1]);
+//     $('#next').text('');
+//     $('#answer').text('');
+//     $('#number').text(n++);
+// }
+
+// function new3() {
+//     runTimer();
+//     $('#question').text(q[2]);
+//     $('#option1').html(a1[2]);
+//     $('#option2').html(a2[2]);
+//     $('#option3').html(a3[2]);
+//     $('#option4').html(a4[2]);
+//     $('#next').text('');
+//     $('#answer').text('');
+//     $('#number').text(n++);
+// }
+
+// function new4() {
+//     runTimer();
+//     $('#question').text(q[3]);
+//     $('#option1').html(a1[3]);
+//     $('#option2').html(a2[3]);
+//     $('#option3').html(a3[3]);
+//     $('#option4').html(a4[3]);
+//     $('#next').text('');
+//     $('#answer').text('');
+//     $('#number').text(n++);
+// }
+
+// function new5() {
+//     runTimer();
+//     $('#question').text(q[4]);
+//     $('#option1').html(a1[4]);
+//     $('#option2').html(a2[4]);
+//     $('#option3').html(a3[4]);
+//     $('#option4').html(a4[4]);
+//     $('#next').text('');
+//     $('#answer').text('');
+//     $('#number').text(n++);
+// }
+
+// function end() {
+//     $('#message').html("You got " + score + " right!<br>You got " + wrong + " wrong!");
+//     $('#answer').text('');
+//     $('#question').text('');
+//     $('#option1').text('');
+//     $('#option2').text('');
+//     $('#option3').text('');
+//     $('#option4').text('');
+//     $('#next').html("<button class=\"blueButton\" onclick=\"repeat()\">Play Again</button>");
+// }
 
 function repeat() {
     location.reload();
 }
+  
+var number = 30;
 
+var intervalId = 0;
 
+function runTimer() {
+    if (number > 0) {
+        stopTimer();
+    } else {
+        number= 30;
+    }
+    intervalId = setInterval(decrement, 1000);
+}
 
+function decrement() {
+    number--;
+    $("#show-number").html(number);
+    if (number <= 0) {
+        stopTimer();
+        timesUpMessage();
+    }
+}
+
+function stopTimer() {
+    clearInterval(intervalId);
+}
+
+function timesUpMessage() {
+    $('#winLoseDisplay').html("<img class=\"img-fluid\" src=\"assets/images/out-of-time.jpg\">");
+    $('#winLoseDisplay').show(0).delay(3000).hide(0);
+    
+    setTimeout(function(){
+        location.reload();
+    }, 3000);
+}
